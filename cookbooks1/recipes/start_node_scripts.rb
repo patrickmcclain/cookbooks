488f9d7f63a0d4f3f5129c98ddc3c8dd855552f3
testaccount7 <klever.aws@gmail.com>
2013-07-25 14:10:24 -0500

    commit

--- cookbooks1/recipes/start_node_scripts.rb
+++ cookbooks1/recipes/start_node_scripts.rb
@@ -4,9 +4,9 @@ bash "start_scripts" do
   code <<-EOH
   cd /srv/www/node_development/current
   if [ -f decider.js ] && [ -f activityTypes.js ] && [ -f node.js ]; then
-  	sudo forever start -w -l forever.log -o out.log -e err.log node.js
-    sudo forever start -w -l forever.log -o out.log -e err.log decider.js
-    sudo forever start -w -l forever.log -o out.log -e err.log activityTypes.js
+  	sudo forever start -w -l -a forever.log -o out.log -e err.log node.js
+    sudo forever start -w -l -a forever.log -o out.log -e err.log decider.js
+    sudo forever start -w -l -a forever.log -o out.log -e err.log activityTypes.js
   fi
   EOH
 end
