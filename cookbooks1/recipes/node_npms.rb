b98addc94f05555d0a6f7aa178e5a50b9e3b4d47
testaccount7 <klever.aws@gmail.com>
2013-07-25 14:02:07 -0500

    update

--- cookbooks1/recipes/node_npms.rb
+++ cookbooks1/recipes/node_npms.rb
@@ -3,15 +3,16 @@ bash "install_node" do
   cwd "/home/ec2-user"
   code <<-EOH
   sudo npm install forever -g
+  sudo npm install aws2js
   sudo npm install memcached
   sudo npm install mongodb
   sudo npm install pubnub
   sleep 120
   cd /srv/www/node_development/current
   if [ -f decider.js ] && [ -f activityTypes.js ] && [ -f node.js ]; then
-  	sudo forever start -w -l forever.log -o out.log -e err.log node.js
-    sudo forever start -w -l forever.log -o out.log -e err.log decider.js
-    sudo forever start -w -l forever.log -o out.log -e err.log activityTypes.js
+  	sudo forever start -w -l -a forever.log -o out.log -e err.log node.js
+    sudo forever start -w -l -a forever.log -o out.log -e err.log decider.js
+    sudo forever start -w -l -a forever.log -o out.log -e err.log activityTypes.js
   fi
   EOH
 end
