200f7dbbd74cd498da016d29c273fdbdd010593b
testaccount7 <klever.aws@gmail.com>
2013-07-25 15:20:49 -0500

    updated forever

--- cookbooks1/recipes/node_npms.rb
+++ cookbooks1/recipes/node_npms.rb
@@ -3,6 +3,7 @@ bash "install_node" do
   cwd "/home/ec2-user"
   code <<-EOH
   sudo npm install forever -g
+  sudo npm install aws-sdk
   sudo npm install aws2js
   sudo npm install memcached
   sudo npm install mongodb
@@ -10,9 +11,9 @@ bash "install_node" do
   cd /srv/www/node_development/current
   if [ -f decider.js ] && [ -f activityTypes.js ] && [ -f node.js ]; then
   	sudo forever stopall
-  	sudo forever start -w -l -a forever.log -o out.log -e err.log node.js
-    sudo forever start -w -l -a forever.log -o out.log -e err.log decider.js
-    sudo forever start -w -l -a forever.log -o out.log -e err.log activityTypes.js
+  	sudo forever start -w -a -l /srv/www/node_development/current/forever.log node.js -o /srv/www/node_development/current/out.log -e /srv/www/node_development/current/err.log node.js
+	sudo forever start -w -a -l /srv/www/node_development/current/forever.log node.js -o /srv/www/node_development/current/out.log -e /srv/www/node_development/current/err.log decider.js
+	sudo forever start -w -a -l /srv/www/node_development/current/forever.log node.js -o /srv/www/node_development/current/out.log -e /srv/www/node_development/current/err.log activityTypes.js
   fi
   EOH
 end
