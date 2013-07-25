fd34b4ec2459b4c455efaf6f66e552965c7bb91b
testaccount7 <klever.aws@gmail.com>
2013-07-25 14:12:36 -0500

    commit

--- cookbooks1/recipes/php_mongo.rb
+++ cookbooks1/recipes/php_mongo.rb
@@ -2,6 +2,7 @@ bash "install_something" do
   user "root"
   cwd "/home/ec2-user"
   code <<-EOH
+  sudo yum install telnet
   sudo pecl install mongo
   cd /etc
   sudo sed -i -e '5iextension=mongo.so\' php.ini
