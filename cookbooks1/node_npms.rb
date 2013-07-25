e5fe026537d1c66afc04246c9c8c4c0414632a8e
testaccount7 <klever.aws@gmail.com>
2013-07-25 02:09:56 -0500

    commit

--- cookbooks1/node_npms.rb
+++ cookbooks1/node_npms.rb
@@ -2,6 +2,7 @@ bash "install_node" do
   user "root"
   cwd "/home/ec2-user"
   code <<-EOH
+  sudo npm install forever -g
   sudo npm install memcached
   sudo npm install mongodb
   sudo npm install pubnub
