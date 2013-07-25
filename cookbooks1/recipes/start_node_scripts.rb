e90b10ff0092146d4db9eefed30907f6c1ee435e
testaccount7 <klever.aws@gmail.com>
2013-07-25 12:15:54 -0500

    commit start_node_scripts

--- cookbooks1/recipes/start_node_scripts.rb
+++ cookbooks1/recipes/start_node_scripts.rb
@@ -1,12 +1,7 @@
-bash "install_node" do
+bash "start_scripts" do
   user "root"
   cwd "/home/ec2-user"
   code <<-EOH
-  sudo npm install forever -g
-  sudo npm install memcached
-  sudo npm install mongodb
-  sudo npm install pubnub
-  sleep 120
   cd /srv/www/node_development/current
   if [ -f decider.js ] && [ -f activityTypes.js ] && [ -f node.js ]; then
   	sudo forever start -w -l forever.log -o out.log -e err.log node.js
