8b5e78bbd0878ea4b62061f57d8369c85a8962ba
testaccount7 <klever.aws@gmail.com>
2013-07-26 16:35:04 -0500

    added stripe to node setup

--- cookbooks1/recipes/node_npms.rb
+++ cookbooks1/recipes/node_npms.rb
@@ -8,6 +8,7 @@ bash "install_node" do
   sudo npm install memcached
   sudo npm install mongodb
   sudo npm install pubnub
+  sudo npm install stripe
   cd /srv/www/node_development/current
   if [ -f decider.js ] && [ -f activityTypes.js ] && [ -f node.js ]; then
   	sudo forever stopall
