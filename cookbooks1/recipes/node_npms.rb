f1873acf92d45815b5d2f9e1e203c27c3a5f9c5c
testaccount7 <klever.aws@gmail.com>
2013-07-26 00:52:56 -0500

    removed watch
    because it causes the script to restart everytime the logs are updated

--- cookbooks1/recipes/node_npms.rb
+++ cookbooks1/recipes/node_npms.rb
@@ -11,9 +11,9 @@ bash "install_node" do
   cd /srv/www/node_development/current
   if [ -f decider.js ] && [ -f activityTypes.js ] && [ -f node.js ]; then
   	sudo forever stopall
-  	sudo forever start -w -a -l /srv/www/node_development/current/forever.log node.js -o /srv/www/node_development/current/out.log -e /srv/www/node_development/current/err.log node.js
-	sudo forever start -w -a -l /srv/www/node_development/current/forever.log node.js -o /srv/www/node_development/current/out.log -e /srv/www/node_development/current/err.log decider.js
-	sudo forever start -w -a -l /srv/www/node_development/current/forever.log node.js -o /srv/www/node_development/current/out.log -e /srv/www/node_development/current/err.log activityTypes.js
+  	sudo forever start -a -l /srv/www/node_development/current/forever.log node.js -o /srv/www/node_development/current/out.log -e /srv/www/node_development/current/err.log node.js
+	sudo forever start -a -l /srv/www/node_development/current/forever.log node.js -o /srv/www/node_development/current/out.log -e /srv/www/node_development/current/err.log decider.js
+	sudo forever start -a -l /srv/www/node_development/current/forever.log node.js -o /srv/www/node_development/current/out.log -e /srv/www/node_development/current/err.log activityTypes.js
   fi
   EOH
 end
--- cookbooks1/recipes/start_node_scripts.rb
+++ cookbooks1/recipes/start_node_scripts.rb
@@ -5,9 +5,9 @@ bash "start_node_scripts" do
   cd /srv/www/node_development/current
   if [ -f decider.js ] && [ -f activityTypes.js ] && [ -f node.js ]; then
   	sudo forever stopall
-  	sudo forever start -w -a -l /srv/www/node_development/current/forever.log node.js -o /srv/www/node_development/current/out.log -e /srv/www/node_development/current/err.log node.js
-	sudo forever start -w -a -l /srv/www/node_development/current/forever.log node.js -o /srv/www/node_development/current/out.log -e /srv/www/node_development/current/err.log decider.js
-	sudo forever start -w -a -l /srv/www/node_development/current/forever.log node.js -o /srv/www/node_development/current/out.log -e /srv/www/node_development/current/err.log activityTypes.js
+  	sudo forever start -a -l /srv/www/node_development/current/forever.log node.js -o /srv/www/node_development/current/out.log -e /srv/www/node_development/current/err.log node.js
+	sudo forever start -a -l /srv/www/node_development/current/forever.log node.js -o /srv/www/node_development/current/out.log -e /srv/www/node_development/current/err.log decider.js
+	sudo forever start -a -l /srv/www/node_development/current/forever.log node.js -o /srv/www/node_development/current/out.log -e /srv/www/node_development/current/err.log activityTypes.js
   fi
   EOH
 end
