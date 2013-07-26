b2440a6f03473646c14025df8c2e4318d2c09d7b
testaccount7 <klever.aws@gmail.com>
2013-07-26 18:00:09 -0500

    fix

--- cookbooks1/recipes/node_npms.rb
+++ cookbooks1/recipes/node_npms.rb
@@ -12,6 +12,7 @@ bash "install_node" do
   cd /srv/www/node_development/current
   if [ -f decider.js ] && [ -f activityTypes.js ] && [ -f node.js ]; then
   	sudo forever stopall
+  	sudo kill -9 node
   	sudo forever start -a -l /srv/www/node_development/current/forever.log node.js -o /srv/www/node_development/current/out.log -e /srv/www/node_development/current/err.log node.js
 	sudo forever start -a -l /srv/www/node_development/current/forever.log node.js -o /srv/www/node_development/current/out.log -e /srv/www/node_development/current/err.log decider.js
 	sudo forever start -a -l /srv/www/node_development/current/forever.log node.js -o /srv/www/node_development/current/out.log -e /srv/www/node_development/current/err.log activityTypes.js
