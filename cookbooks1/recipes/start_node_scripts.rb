06dfc81bb016bb6abb258ea039404a5a225607de
testaccount7 <klever.aws@gmail.com>
2013-07-26 20:04:46 -0500

    fixed start scripts

--- cookbooks1/recipes/start_node_scripts.rb
+++ cookbooks1/recipes/start_node_scripts.rb
@@ -4,13 +4,13 @@ bash "start_node_scripts" do
   code <<-EOH
   cd /srv/www/node_development/current
   if [ -f decider.js ] && [ -f activityTypes.js ] && [ -f node.js ]; then
-  	sleep 120
+  	sudo -s
   	sudo forever stopall
   	sudo kill -9 node
   	sudo kill -9 sudo
-  	sudo forever start -a -l /srv/www/node_development/current/forever.log node.js -o /srv/www/node_development/current/out.log -e /srv/www/node_development/current/err.log node.js
-	sudo forever start -a -l /srv/www/node_development/current/forever.log node.js -o /srv/www/node_development/current/out.log -e /srv/www/node_development/current/err.log decider.js
-	sudo forever start -a -l /srv/www/node_development/current/forever.log node.js -o /srv/www/node_development/current/out.log -e /srv/www/node_development/current/err.log activityTypes.js
+  	forever start -a -l /srv/www/node_development/current/forever.log node.js -o /srv/www/node_development/current/out.log -e /srv/www/node_development/current/err.log node.js
+	forever start -a -l /srv/www/node_development/current/forever.log node.js -o /srv/www/node_development/current/out.log -e /srv/www/node_development/current/err.log decider.js
+	forever start -a -l /srv/www/node_development/current/forever.log node.js -o /srv/www/node_development/current/out.log -e /srv/www/node_development/current/err.log activityTypes.js
   fi
   EOH
 end
