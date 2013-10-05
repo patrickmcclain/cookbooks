e8721d4bf695780214d53b871e215953fdbb619a
testaccount7 <klever.aws@gmail.com>
2013-10-05 01:32:26 -0500

    fix

--- cookbooks1/recipes/start_node_scripts_production.rb
+++ cookbooks1/recipes/start_node_scripts_production.rb
@@ -1,18 +1,18 @@
 bash "start_node_scripts" do
   user "root"
-  cwd "/srv/www/node_development/current"
+  cwd "/srv/www/back_end/current"
   code <<-EOH
-  cd /srv/www/node_development/current
+  cd /srv/www/back_end/current
   if [ -f decider.js ] && [ -f activityTypes.js ] && [ -f node.js ] && [ -f image_activityTypes.js ] && [ -f image_decider.js ]; then
   	forever stopall
 
   	sudo -s
   	
-  	forever start -a -l /srv/www/node_development/current/forever.log -a -o /srv/www/node_development/current/out.log -a -e /srv/www/node_development/current/err.log node.js
-	forever start -a -l /srv/www/node_development/current/forever.log -a -o /srv/www/node_development/current/out.log -a -e /srv/www/node_development/current/err.log decider.js
-	forever start -a -l /srv/www/node_development/current/forever.log -a -o /srv/www/node_development/current/out.log -a -e /srv/www/node_development/current/err.log activityTypes.js
-	forever start -a -l /srv/www/node_development/current/forever.log -a -o /srv/www/node_development/current/out.log -a -e /srv/www/node_development/current/err.log image_activityTypes.js
-	forever start -a -l /srv/www/node_development/current/forever.log -a -o /srv/www/node_development/current/out.log -a -e /srv/www/node_development/current/err.log image_decider.js
+  	forever start -a -l /srv/www/back_end/current/forever.log -a -o /srv/www/back_end/current/out.log -a -e /srv/www/back_end/current/err.log node.js
+	forever start -a -l /srv/www/back_end/current/forever.log -a -o /srv/www/back_end/current/out.log -a -e /srv/www/back_end/current/err.log decider.js
+	forever start -a -l /srv/www/back_end/current/forever.log -a -o /srv/www/back_end/current/out.log -a -e /srv/www/back_end/current/err.log activityTypes.js
+	forever start -a -l /srv/www/back_end/current/forever.log -a -o /srv/www/back_end/current/out.log -a -e /srv/www/back_end/current/err.log image_activityTypes.js
+	forever start -a -l /srv/www/back_end/current/forever.log -a -o /srv/www/back_end/current/out.log -a -e /srv/www/back_end/current/err.log image_decider.js
   fi
   EOH
 end
