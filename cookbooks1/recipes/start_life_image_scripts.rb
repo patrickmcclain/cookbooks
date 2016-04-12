e0c6aca8222346fd5d391dc3dba9462165fa7170
testaccount7 <klever.aws@gmail.com>
2016-04-11 23:23:58 -0500

    splitting up some cookbooks

--- cookbooks1/recipes/start_life_image_scripts.rb
+++ cookbooks1/recipes/start_life_image_scripts.rb
@@ -1,22 +1,15 @@
-bash "install_node" do
+bash "start_life_node_scripts" do
   user "root"
-  cwd "/home/ec2-user"
+  cwd "/srv/www/back_end/current"
   code <<-EOH
-  yum -y install GraphicsMagick.x86_64
-  npm install forever -g
-  npm install aws-sdk
-  npm install express
-  npm install orientjs
-  npm install request
-  npm install body-parser
   cd /srv/www/back_end/current
-  if [ -f test.js ] && [ -f image_activityTypes.js ] && [ -f image_decider.js ]; then
+  if [ -f image_processor.js ] && [ -f image_activityTypes.js ] && [ -f image_decider.js ]; then
   	forever stopall
     kill -9 node
     kill -9 sudo
   	sudo -s
   	
-  	forever start -a -l /srv/www/back_end/current/forever.log -a -o /srv/www/back_end/current/out.log -a -e /srv/www/back_end/current/err.log test.js
+  	forever start -a -l /srv/www/back_end/current/forever.log -a -o /srv/www/back_end/current/out.log -a -e /srv/www/back_end/current/err.log image_processor.js
   	forever start -a -l /srv/www/back_end/current/forever.log -a -o /srv/www/back_end/current/out.log -a -e /srv/www/back_end/current/err.log image_decider.js
   	forever start -a -l /srv/www/back_end/current/forever.log -a -o /srv/www/back_end/current/out.log -a -e /srv/www/back_end/current/err.log image_activityTypes.js
   fi
---
+++ cookbooks1/recipes/start_life_solo_scripts.rb
@@ -0,0 +1,15 @@
+bash "start_life_node_scripts" do
+  user "root"
+  cwd "/srv/www/back_end/current"
+  code <<-EOH
+  cd /srv/www/back_end/current
+  if [ -f test.js ]; then
+  	forever stopall
+    kill -9 node
+    kill -9 sudo
+  	sudo -s
+  	
+  	forever start -a -l /srv/www/back_end/current/forever.log -a -o /srv/www/back_end/current/out.log -a -e /srv/www/back_end/current/err.log test.js
+  fi
+  EOH
+end
