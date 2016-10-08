e1cd54e9abe021101e08245608b0f074f456bb20
testaccount7 <klever.aws@gmail.com>
2016-10-08 07:31:15 -0500

    Revert "updates"
    This reverts commit c24cedfe342400682e2513fd046331d1bd492d55.

--- cookbooks1/recipes/start_life_image_scripts.rb
+++ cookbooks1/recipes/start_life_image_scripts.rb
@@ -5,8 +5,8 @@ bash "start_life_node_scripts" do
   cd /srv/www/back_end/current
   if [ -f image_processor.js ] && [ -f image_activityTypes.js ] && [ -f image_decider.js ]; then
   	forever stopall
-    killall node
-    killall sudo
+    kill -9 node
+    kill -9 sudo
   	sudo -s
   	
   	forever start -a -l /srv/www/back_end/current/forever.log -a -o /srv/www/back_end/current/out.log -a -e /srv/www/back_end/current/err.log image_processor.js
