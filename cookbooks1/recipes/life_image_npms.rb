c24cedfe342400682e2513fd046331d1bd492d55
testaccount7 <klever.aws@gmail.com>
2016-10-08 07:13:27 -0500

    updates

--- cookbooks1/recipes/life_image_npms.rb
+++ cookbooks1/recipes/life_image_npms.rb
@@ -12,8 +12,8 @@ bash "install_node" do
   cd /srv/www/back_end/current
   if [ -f image_processor.js ] && [ -f image_activityTypes.js ] && [ -f image_decider.js ]; then
   	forever stopall
-    kill -9 node
-    kill -9 sudo
+    killall node
+    killall sudo
   	sudo -s
   	
   	forever start -a -l /srv/www/back_end/current/forever.log -a -o /srv/www/back_end/current/out.log -a -e /srv/www/back_end/current/err.log image_processor.js
